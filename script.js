540ec17d3646599b2da170d2e7989ee1c4ba0c52
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-03-19 11:18:24 +0100

    ajout de l'évènement qui attribue les nombres et le signe d'opération

--- script.js
+++ script.js
@@ -1,38 +1,71 @@
 /// Mes sélecteurs
 const digit = document.querySelector(".digits");
+const operator = document.querySelector(".operators");
+const operations = document.querySelector(".for-operations");
 const screen = document.querySelector(".screen");
 let isNumberAdded = false;
 let num1;
 let num2;
+let sign;
+let target;
+
+/// Evènement pour attribuer les nombres et l'opérateur
+operations.addEventListener("click", (e) => {
+  target = e.target;
+  if (target.matches("button")) {
+    if (target.matches(".digit")) {
+      getNumber(e);
+    } else {
+      if (target.matches(".op")) {
+        getOperationSign(e);
+      } else {
+        if (sign === undefined) {
+          num1 = screen.textContent;
+          screen.textContent = "0";
+          console.log("num1 = " + num1);
+        } else {
+          num2 = screen.textContent;
+          screen.textContent = "0";
+          console.log("num2 = " + num2);
+        }
+      }
+    }
+  }
+});
 
 /// Fonction pour update les nombres lorsqu'on clique sur un bouton de chiffre
-function getNumber() {
-  digit.addEventListener("click", (e) => {
-    let target = e.target;
-    let number;
+function getNumber(event) {
+  target = event.target;
 
-    if (target.matches("button")) {
-      number = target.textContent;
-      console.log(`hors condition ${number}`);
+  if (target.textContent === "=") {
+    number = "";
+  } else {
+    number = target.textContent;
 
-      if (screen.textContent === "0") {
-        screen.textContent = number;
-      } else {
+    // Si le nombre affiché est 0
+    if (screen.textContent === "0") {
+      if (number === ".") {
         screen.textContent += number;
+      } else {
+        screen.textContent = number;
       }
-      number = screen.textContent;
-      console.log("final number " + number);
+    } else {
+      screen.textContent += number;
     }
-
-    return number;
-  });
+    number = screen.textContent;
+    console.log(`number : ${number}`);
+  }
 }
 
-function getOperationSign() {
-  
-}
+/// Fonction pour savoir quel signe d'opération a été choisit
+function getOperationSign(event) {
+  target = event.target;
 
-getNumber();
+  if (target.matches(".op")) {
+    sign = target.textContent;
+  }
+  console.log("sign = " + sign);
+}
 
 /// Fonction d'addition
 function add(num1, num2) {
